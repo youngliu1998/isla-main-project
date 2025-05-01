36dfe4a6b31c50a3fc647380d257ccf95844de00
Lotte <nieves957@gmail.com>
2025-05-01 11:57:12 +0800

    Lotte/set_BS_variables

---
+++ front-end/app/cart/order-completed/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import React, { useState, useEffect } from 'react'
+
+export default function OrderCompletedPage(props) {
+  return (
+    <>
+      <div>OrderCompleted Page</div>
+    </>
+  )
+}
