c530dcede63dd787876a56e827a1bbcb3d1b486b
Young <youngliu1998@gmail.com>
2025-05-07 00:52:58 +0800

    I need more kownlegde for db

--- back-end/routes/member.js
+++ back-end/routes/member.js
@@ -1,19 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { getUsers } from '../services/user.js'
-import { successResponse, errorResponse, isDev } from '../lib/utils.js'
 import db from '../config/mysql.js'
 
 /* GET home page. */
-router.get('/', async (req, res) => {
-  try {
-    // 需要加上await等待取得資料
-    const [users] = await db.query('SELECT * FROM `user` LIMIT 100')
-    // successResponse(res, { users })
-    res.json({ status: 'success', data: users })
-  } catch (error) {
-    errorResponse(res, error)
-  }
+router.get('/', async function (req, res) {
+
+  const [members] = await db.query(`SELECT * FROM user`)
+  res.json({ status: 'success', data: members })
 })
 
 export default router
