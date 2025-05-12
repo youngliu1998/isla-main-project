76afa54ea0b2f6df3d6a88d5bf217ce4930cbc88
Lotte <nieves957@gmail.com>
2025-05-13 02:16:11 +0800

    0512_step01-components

--- front-end/app/cart/_component/product-card/product-card.js
+++ front-end/app/cart/_component/product-card/product-card.js
@@ -1,6 +1,8 @@
 import ProductCardDropdown from './product-card-dropdown'
 import ProductCardColorDots from './product-card-color-dots'
 import ProductCardAddon from './product-card-addon'
+import ProductCardCourse from './product-card-course'
+import ProductCardTest from './product-card-test'
 
 export default function ProductCard({ type, ...props }) {
   switch (type) {
@@ -12,6 +14,12 @@ export default function ProductCard({ type, ...props }) {
 
     case 'addon':
       return <ProductCardAddon {...props} />
+
+    case 'course':
+      return <ProductCardCourse {...props} />
+
+    case 'test':
+      return <ProductCardTest {...props} />
 
     default:
       return <div>沒有這個樣式的卡片</div>
