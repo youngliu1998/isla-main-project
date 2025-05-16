ea9d2f93b15914cc67b3c052b1f66f3c0116dc1e
Abby <mm2853319@gmail.com>
2025-05-16 11:37:57 +0800

    course banner

--- back-end/routes/course/course.js
+++ back-end/routes/course/course.js
@@ -1,22 +1,39 @@
 import express from 'express'
 const router = express.Router()
 import db from '../../config/mysql.js'
-import verifyToken from '../../lib/verify-token.js' // token verification
 
-// 查詢所有課程與體驗課程
-router.get('/22222', async (req, res) => {
-  const user_id = req.query.user_id
+//course + courses_comments
+router.get('/', async (req, res) => {
   try {
-    const [course] = await db.query(
-      `
-      select * from courses limit 12
-     `
-    )
+    const [course] = await db.query(`
+      SELECT
+        c.*,
+        COALESCE(AVG(cc.star), 0) AS avg_star,
+        COUNT(cc.id) AS comment_count
+      FROM
+        courses c
+      LEFT JOIN
+        courses_comments cc ON c.id = cc.courses_id
+      GROUP BY
+        c.id
+      LIMIT 35
+    `)
 
     res.json({ status: 'success', data: course })
   } catch (err) {
-    console.error('撈取優惠券錯誤:', err)
+    console.error('撈取課程錯誤:', err)
     res.status(500).json({ status: 'false', message: '連接資料庫錯誤' })
+  }
+})
+
+// /api/course/experience
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT * FROM courses_experience')
+    res.json({ status: 'success', data: rows })
+  } catch (err) {
+    console.error('撈取體驗課程失敗:', err)
+    res.status(500).json({ status: 'false', message: '資料庫錯誤' })
   }
 })
 
