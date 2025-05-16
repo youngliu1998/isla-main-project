c2d736f88dcdf1ead84827af4a225f2f40e84ed4
Lotte <nieves957@gmail.com>
2025-05-16 13:13:22 +0800

    fixed_routes

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import 'dotenv/config.js'
 // import productRoutes from './routes/product/products.js'
 
 const app = express()
