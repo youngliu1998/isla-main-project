17b5beaa39637cb2ebf3960a7229c6dd886417b0
Young <youngliu1998@gmail.com>
2025-05-16 11:30:21 +0800

    0516_order-list

--- front-end/app/member/_component/order-list.js
+++ front-end/app/member/_component/order-list.js
@@ -1,34 +1,53 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import OrderProdList from './_order-component/order-prod-list'
 import './_style.css/order-list.css'
 
 export default function OrderList(props) {
+  const [orderTable, setOrderTable] = useState({
+    訂單編號: '12345',
+    購買日期: '2015-01-01',
+    訂單狀態: '',
+    結帳金額: '',
+    付款方式: '',
+    發票號碼: '',
+  })
+  const [openOrder, setOpenOrder] = useState(false)
+  const btnOrderContent = openOrder ? '闔上' : '展開'
+  const orderOpenClass = openOrder ? '' : 'close-order'
   return (
     <>
-      <div className="d-flex flex-md-row flex-column w-100 order-list">
-        <div className="order-col">
-          <div className="order-index">訂單編號</div>
-          <div className="order-content">12345</div>
+      <div
+        className={'d-flex flex-column justify-content-center w-100 order-list'}
+      >
+        <div
+          className={
+            'd-flex flex-md-row flex-column overflow-hidden w-100' +
+            ' ' +
+            orderOpenClass
+          }
+        >
+          {Object.keys(orderTable).map((key, i) => {
+            return (
+              <div className="order-col" key={i}>
+                <div className="order-index">{key}</div>
+                <div className="order-content">{orderTable.key}</div>
+              </div>
+            )
+          })}
         </div>
-        <div className="order-col">
-          <div className="order-index">購買日期</div>
-          <div className="order-content">12345</div>
-        </div>
-        <div className="order-col">
-          <div className="order-index">購買日期</div>
-          <div className="order-content">1234567</div>
-        </div>
-        <div className="order-col">
-          <div className="order-index">購買日期</div>
-          <div className="order-content">12345</div>
-        </div>
-        <div className="order-col">
-          <div className="order-index">購買日期</div>
-        </div>
-        <div className="order-col">
-          <div className="order-index">購買日期</div>
-        </div>
+
+        <OrderProdList />
+        {/* button control open */}
+        <button
+          className="d-md-none d-block btn-order"
+          onClick={() => {
+            setOpenOrder(!openOrder)
+          }}
+        >
+          {btnOrderContent}
+        </button>
       </div>
     </>
   )
