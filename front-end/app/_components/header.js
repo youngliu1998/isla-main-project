8237a440e2db87c8a6c74e0ce80d8f2d539e4db4
song <a0985034245a@gmail.com>
2025-05-21 18:47:53 +0800

    0521

--- front-end/app/_components/header.js
+++ front-end/app/_components/header.js
@@ -25,7 +25,7 @@ export default function Header() {
     }
   }
   const [hamMenuOpen, setHamMenuOpen] = useState(false)
-  // const pathname = usePathname()
+  const pathname = usePathname()
   const { isAuth } = useAuth()
   const loginUrl = isAuth ? 'profile' : 'login'
   if (
