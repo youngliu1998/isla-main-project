f5ad1eda8510df79e3fd93f5dad85932667ae16e
song <a0985034245a@gmail.com>
2025-05-23 01:37:35 +0800

    0523 coupon updated

---
+++ front-end/app/dashboard/layout.js
@@ -0,0 +1,23 @@
+import Header from './_components/header'
+import PcSidebar from './_components/pc-sidebar'
+import Footer from './_components/footer'
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div style={{ marginTop: '-80px' }}>
+      <Header />
+
+      <div className="container-fluid">
+        <div className="row">
+          <aside className="d-none d-lg-block col-lg-2 bg-white border-end px-3 py-4">
+            <PcSidebar />
+          </aside>
+
+          <main className="col-lg-10 col-md-12 p-4">{children}</main>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
---
+++ front-end/app/dashboard/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function DashboardPage() {
+  return (
+    <>
+      <h1>後臺首頁</h1>
+    </>
+  )
+}
