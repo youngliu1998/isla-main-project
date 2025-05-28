3e550d0559d04e7bd8decc5bbe340e81f5f2a9a8
Young <youngliu1998@gmail.com>
2025-05-28 15:30:00 +0800

    order 80%

--- front-end/app/member/order/page.js
+++ front-end/app/member/order/page.js
@@ -1,35 +1,58 @@
 'use client'
 
 import '../_component/_style.css/form.css'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import OrderList from './_component/order-list'
 
 export default function OrderPage() {
+  const [orders, setOrders] = useState([])
+
+  // ==== 取得訂單 ====
+  async function getOrder() {
+    try {
+      const token = localStorage.getItem('jwtToken')
+
+      const response = await fetch('http://localhost:3005/api/member/order', {
+        method: 'GET',
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      const data = await response.json()
+      console.log(data)
+      if (response.ok) {
+        setOrders(data['data'])
+      } else {
+        console.log('訂單資料表-連線錯誤')
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  console.log('orders', orders)
   useEffect(() => {
     const token = localStorage.getItem('jwtToken') || null
     if (!token) return
-    // async function getOrder() {
-    //   try {
-    //     const token = localStorage.getItem('jwtToken')
 
-    //     const response = await fetch(
-    //       'http://localhost:3005/api/member/order',
-    //       {
-    //         method: 'GET',
-    //         headers: { Authorization: `Bearer ${token}` },
-    //       }
-    //     )
-    //   } catch (err) {
-    //     console.log(err)
-    //   }
-    // }
-    // getOrder()
+    getOrder()
   }, [])
   return (
     <>
       <div className="user-content">
         <h3>訂單詳細資料</h3>
-        <OrderList />
+        {orders.map((order, i) => {
+          return (
+            <OrderList
+              key={i}
+              order_number={order['order_number']}
+              created_at={order['created_at']}
+              status={order['status']}
+              total_price={order['total_price']}
+              shipping_method={order['shipping_method']}
+              payment_method={order['payment_method']}
+              order_id={order['id']}
+            />
+          )
+        })}
       </div>
     </>
   )
