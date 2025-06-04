1728c0e9f61748d33961ddd8607d6b63ff543357
Young <youngliu1998@gmail.com>
2025-06-04 17:38:38 +0800

    header search bar

--- front-end/app/_components/header/_component/search-block/_component/forum-bar.js
+++ front-end/app/_components/header/_component/search-block/_component/forum-bar.js
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 // ==== route ====
 import { USER_AVA_URL } from '@/_route/img-url'
+// ==== css ====
+import '../_style/forum.css' // 卡片css
 
 export default function ForumBar({ forum = {} }) {
   return (
@@ -11,7 +13,7 @@ export default function ForumBar({ forum = {} }) {
       {/* ==== card ==== */}
       <div className="header-search-item for-forum">
         <div className="d-flex flex-column justify-content-between gap-2">
-          {/* ==== user-info ==== */}
+          {/* ==== auth-info ==== */}
           <div className="d-flex gap-2">
             <div className="search-item-img rounded-pill overflow-hidden">
               <Image
@@ -29,10 +31,11 @@ export default function ForumBar({ forum = {} }) {
             <Link href="">{forum.title.slice(0, 10)}</Link>
           </div>
         </div>
+        {/* ==== Content ==== */}
         <div
           className="post-content text-truncate fs14 sub-text-color px-4"
           dangerouslySetInnerHTML={{
-            __html: forum.content.replace('<br/>', ' ').slice(0, 80),
+            __html: forum.content?.replace('<br/>', ' ').slice(0, 80) || '',
           }}
         />
       </div>
