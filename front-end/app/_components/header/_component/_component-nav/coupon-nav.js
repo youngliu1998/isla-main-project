aa070731032975e10079661c8d6467c2428c421f
Young <youngliu1998@gmail.com>
2025-06-05 16:37:39 +0800

    conflict coupon-nav

--- front-end/app/_components/header/_component/_component-nav/coupon-nav.js
+++ front-end/app/_components/header/_component/_component-nav/coupon-nav.js
@@ -2,12 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-<<<<<<< HEAD
-// ==== css ====
-import './_style/coupon-nav.css'
-=======
 import './coupon-nav.css'
->>>>>>> dev
 
 export default function CouponNav({ open = false }) {
   const isOpen = open ? 'd-flex' : 'd-none'
@@ -15,11 +10,7 @@ export default function CouponNav({ open = false }) {
     <>
       <div
         className={
-<<<<<<< HEAD
-          'flex-column align-items-center gap-2 py-2 px-1 position-absolute overflow-hidden nav-coupon-selection' +
-=======
           'coupon-nav-menu-box flex-column align-items-center position-absolute overflow-hidden' +
->>>>>>> dev
           ' ' +
           isOpen
         }
