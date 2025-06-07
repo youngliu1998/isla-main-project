69c2d381df41261d8d1fb6fc6f1dd38cfd28219d
song <a0985034245a@gmail.com>
2025-06-08 04:41:48 +0800

    [fix member coupon]0608T0441

--- front-end/app/coupon/_components/pc-nav.js
+++ front-end/app/coupon/_components/pc-nav.js
@@ -3,6 +3,7 @@ import IslaSwitch from '../../_components/form/switch/form-switch'
 import './coupon.css'
 
 export default function CouponFilterNav({
+  type = '', // 給會員中心-我的優惠券用
   options = [], // li選項
   currentValue, // 當前選擇的值加上active
   onChange, // 切換選項時的處理函式
@@ -10,9 +11,14 @@ export default function CouponFilterNav({
   isChecked = false, // 開關是否打開
   onToggleSwitch = () => {}, // 點擊開關時
 }) {
+  const isMember = type === 'member'
   return (
     <ul
-      className={`sub-text nav d-none d-md-flex justify-content-center justify-content-md-start mt-3`}
+      className={`sub-text nav justify-content-center justify-content-md-start mt-3 ${
+        isMember
+          ? 'd-flex flex-wrap justify-content-center'
+          : 'd-none d-md-flex justify-content-md-start'
+      }`}
     >
       {options.map((item) => (
         <li
